3577b14a2e70aba736ad935545904086bb131cf2
Abdelslam El-kholy <abdelslam.ahmed.elkholy@gmail.com>
2022-08-11 21:10:50 +0200

    first commit

---
+++ src/formSource.js
@@ -0,0 +1,98 @@
+export const userInputs = [
+  {
+    id: "name",
+    label: "name",
+    type: "text",
+    placeholder: "Abdelslam Elkholy",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "abdelslam@gmail.com",
+  },
+
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+];
+
+export const hotelInputs = [
+  {
+    id: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "My Hotel",
+  },
+  {
+    id: "type",
+    label: "Type",
+    type: "text",
+    placeholder: "hotel",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "Cairo",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+    placeholder: "Talaat Harb st, 66",
+  },
+  {
+    id: "distance",
+    label: "Distance from City Center",
+    type: "text",
+    placeholder: "500",
+  },
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "The best Hotel",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "description",
+  },
+  {
+    id: "cheapestPrice",
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+];
+
+export const roomInputs = [
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "Double room",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "Double size bed, 1 bathroom",
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: "number",
+    placeholder: "100",
+  },
+  {
+    id: "maxPeople",
+    label: "Max People",
+    type: "number",
+    placeholder: "2",
+  },
+];
---
+++ src/pages/New/NewUser.jsx
@@ -0,0 +1,87 @@
+import "./new.scss";
+import { Sidebar, Navbar } from "../../components";
+import { DriveFolderUploadOutlinedIcon } from "../../MUI/icons";
+import { useState } from "react";
+import axios from "axios";
+import { userInputs } from "../../formSource";
+import { useNavigate } from "react-router-dom";
+
+const NewUser = () => {
+  const [file, setFile] = useState("");
+  const [info, setInfo] = useState({});
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "upload");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/lamadev/image/upload",
+        data
+      );
+
+      const { url } = uploadRes.data;
+
+      const newUser = {
+        ...info,
+        img: url,
+      };
+
+      await axios.post("/register", newUser);
+
+      navigate("/users");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(info);
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left"></div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              {userInputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input
+                    onChange={handleChange}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    id={input.id}
+                  />
+                </div>
+              ))}
+              <button onClick={handleClick}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewUser;
